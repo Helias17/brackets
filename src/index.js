b1346d09035032d885e6f78408fd131610da21e3
Ilya Komar <helias@yandex.ru>
2020-10-16 03:01:13 +0300

    init: start task 'brackets'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if ( str.length % 2 !== 0 ) {
+    //alert('Не подходит по длине');
+    return false;
+  }
+
+  let newArr = bracketsConfig.map( (item) => {
+    return item[0]+item[1];
+  } )
+
+  //alert(newArr);
+
+  let strLen = str.length/2;
+
+  for (let i=0; i < strLen; i++) {
+
+    newArr.map( (item) => {
+        str = str.replace(item, '');
+    } )
+
+    //alert(`str.length: ${strLen}, str: ${str}, i: ${i} `);
+  }
+
+    if (str.length == 0) {
+    //alert('Подходит!');
+    return true;
+  } else {
+    //alert('Не подходит!');
+    return false;
+  }
+
 }
